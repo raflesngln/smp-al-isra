22bdfe919084842a3e6902bdf1a3e46967642590
raflesngln <raflesngln@gmail.com>
2017-08-13 15:50:39 +0700

    up

--- smp_al_isra.sql
+++ smp_al_isra.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.5-10.1.13-MariaDB : Database - smpbinautama
+MySQL - 5.5.5-10.1.13-MariaDB : Database - smp_al_isra
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.5.5-10.1.13-MariaDB : Database - smpbinautama
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`smpbinautama` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`smp_al_isra` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `smpbinautama`;
+USE `smp_al_isra`;
 
 /*Table structure for table `admin` */
 
